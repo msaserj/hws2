33a8430f7811c163b73189e35d2d5fa0c42369e1
msa <serg.ks@gmail.com>
2023-01-06 01:35:29 +0400

    hw4: fifth try

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,7 +17,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button + (disabled ? s.disabled : xType === 'red' ? s.red + (className ? ' ' + className : s.secondary) : s.default) // задачка на смешивание классов
+    const finalClassName = s.button + (disabled ? s.disabled : xType === 'red' ? s.red + (className ? ' ' + className : s.default) : s.secondary) // задачка на смешивание классов
     //const finalClassName = s.button
         // + (disabled
         //         ? ...
