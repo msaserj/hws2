6bb4083ec440c816c4c888f6734b69033a66e51f
msa <serg.ks@gmail.com>
2023-01-06 16:11:05 +0400

    hw9: second try plus

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -39,8 +39,8 @@ function Clock() {
     const stringDate = date?.toLocaleDateString('ru-Ru')|| <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = new Date().toLocaleString("en", {weekday: "long"}) || <br/> // пишут студенты
-    const stringMonth = new Date().toLocaleString("en", {month: "long"}) || <br/> // пишут студенты
+    const stringDay = date?.toLocaleString('en-US', {weekday: 'long'}) || <br/> // пишут студенты
+    const stringMonth = date?.toLocaleString('en-US', {month: 'long'}) || <br/> // пишут студенты
 
 
     // const stringTime = date?.toLocaleTimeString() || <br/> // fix with date
