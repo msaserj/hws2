4b9e9d382032230c629e02627905a503994730aa
msa <serg.ks@gmail.com>
2023-01-06 14:57:21 +0400

    hw8: first try

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,32 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+             // need to fix
+            return [...state].sort((a, b) => {
+                if (action.payload === 'up') {
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                } else if (action.payload === 'down') {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                }
+                return 0
+            })
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter(age => age.age > 18) // need to fix
         }
         default:
             return state
